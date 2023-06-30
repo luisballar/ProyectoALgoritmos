d2fb5fc104dc250ed4fcfcd8785a650f1e38beaf
luisballar <luisballar123@gmail.com>
2023-06-30 00:21:10 -1100

    Fragmentador editado

--- Test/model/model/ShotgunTest.java
+++ Test/model/model/ShotgunTest.java
@@ -1,7 +1,9 @@
 package model;
 
 import model.Shotgun;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import service.ArchivoDAO;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -13,19 +15,20 @@ class ShotgunTest {
     Shotgun gun = new Shotgun();
 
     @Test
-    public void prueba_sgotGun_Random(){
-
-        System.out.println(gun.randomText(100));
-
-    }
-    @Test
     public void pruebaLectura() throws IOException {
        // System.out.println();
     }
 
     @Test
-    public void pruebaFragmentador() throws IOException {
-        gun.fragmentador(12,10);
+    public void fragmentadorHacer(){
+
+        try {
+            gun.fragmentador(13, 12);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 
 }
